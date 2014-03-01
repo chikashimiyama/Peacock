e580b5c41c7ba2858f8a0916beaebb42a920ddd3
Chikashi Miyama <chikashimiyama@gmail.com>
2014-03-01 20:49:34 +0100

    SPI - ADC function added

--- Peacock_Hardware/Peacock_Hardware/src/Peacock.h
+++ Peacock_Hardware/Peacock_Hardware/src/Peacock.h
@@ -16,12 +16,83 @@
 #include <LUFA/Drivers/Board/Joystick.h>
 #include <LUFA/Drivers/USB/USB.h>
 #include <LUFA/Platform/Platform.h>
-
 #include "lcd.h"
-#include "spi.h"
 
-void SetupHardware(void);
+
+
+// Peacock sensors
+#define COLUMN 7
+#define ROW 5
+#define NUM_SENSORS COLUMN * ROW
+// LED
+#define LED_PORT PORTC 
+#define LED_DDR	 DDRD
+#define LED_PIN  PORTC4
+
+// BUTTON
+#define BUTTON_BT0_PORT PORTC
+#define BUTTON_BT0_DDR  DDRC
+#define BUTTON_BT0_PIN  PORTC2
+
+#define BUTTON_BT1_PORT PORTD
+#define BUTTON_BT1_DDR  DDRD
+#define BUTTON_BT1_PIN  PORTD0
+
+#define BUTTON_BT2_PORT PORTD
+#define BUTTON_BT2_DDR  DDRD
+#define BUTTON_BT2_PIN  PORTD1
+
+#define BUTTON_BT3_PORT PORTD
+#define BUTTON_BT3_DDR  DDRD
+#define BUTTON_BT3_PIN  PORTD2
+
+// SPI
+#define SPI_PORT PORTB 
+#define SPI_DDR  DDRB
+#define SPI_SCK  PORTB1
+#define SPI_MOSI PORTB2
+#define SPI_MISO PORTB3
+
+#define SPI_CS0_PORT PORTB
+#define SPI_CS0_DDR  DDRB
+#define SPI_CS0_PIN  PORTB0
+ 
+#define SPI_CS1_PORT PORTD
+#define SPI_CS1_DDR  DDRD
+#define SPI_CS1_PIN  PORTD3
+
+#define SPI_CS2_PORT PORTD
+#define SPI_CS2_DDR  DDRD
+#define SPI_CS2_PIN  PORTD4
+
+#define SPI_CS3_PORT PORTD
+#define SPI_CS3_DDR  DDRD
+#define SPI_CS3_PIN  PORTD5
+
+#define SPI_CS4_PORT PORTD
+#define SPI_CS4_DDR  DDRD
+#define SPI_CS4_PIN  PORTD6
+
+#define SPI_SINGLE_MODE_MASK 0x18 // bit 3 4 HI
+
+// protocol
+
+// delimeter
+#define START_DELIMETER 0xFF
+#define END_DELIMETER 0xFE
+
+// type of message
+#define DATA_PACKET 0xFD
+#define COMMAND_PACKET 0xFC
+#define ERROR_PACKET 0xFB
+
+// data max
+#define DATA_MAX 0xFA // 250
+
+void setupHardware(void);
+uint8_t spiReadWrites(uint8_t byte);
 void sendMessage(void);	
+void toggleLED(bool sw);
 void EVENT_USB_Device_Connect(void);
 void EVENT_USB_Device_Disconnect(void);
 void EVENT_USB_Device_ConfigurationChanged(void);
